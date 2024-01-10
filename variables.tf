9bb31a286df8fa296e91f790195af6dfa603bd7c
armdupre <a-joseph.dupre@keysight.com>
2024-01-10 11:08:49 -0500

    Updated.

--- variables.tf
+++ variables.tf
@@ -49,7 +49,7 @@ variable "UserProjectTag" {
 }
 
 variable "Version" {
-	default = "9-36"
+	default = "10-00"
 	description = "Versioning of the application using the deployment"
 	type = string
 }
